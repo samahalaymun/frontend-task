96d17159667150e3287d06f15cdd88a97b3912cc
samahalaymun <samah_abu_laymun@hotmail.com>
2025-10-26 14:07:15 +0300

    delete  index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/app/App";
-import "@/app/index.css";
 import { QueryProvider } from "@/app/providers/QueryProvider";
 import "./App.css";
 
